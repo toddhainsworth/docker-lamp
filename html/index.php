50e12a9c8d8d1f276de7181c3b4788176f89f0c2
Todd Hainsworth <hainsworth.todd@gmail.com>
2017-11-22 08:26:26 +1030

    mysql and mysqli support for php

--- html/index.php
+++ html/index.php
@@ -5,5 +5,21 @@
 <body>
 <h2>Hello world, from the Dockerized LAMP stack!</h2>
 <?php echo "Hello from PHP ;)"; ?>
+
+<?php
+$serverName = 'mysql';
+$userName = 'lamp';
+$password = 'lamp';
+$dbName = 'lamp';
+
+$link = new mysqli($serverName, $userName, $password, $dbName);
+
+if ($link->connect_error) {
+  printf("Connect failed: %s\n", $link->connect_error);
+  exit();
+}
+
+echo "Got a connection to MySQL!";
+?>
 </body>
 </html>
